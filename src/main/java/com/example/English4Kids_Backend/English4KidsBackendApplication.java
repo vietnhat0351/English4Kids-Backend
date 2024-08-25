bc6df69c90d2ca4eb6e2c738bded3d40b8bee59d
Việt Nhật Phạm <drivervietnhat@gmail.com>
2024-08-25 18:45:08 +0700

    init project

---
+++ src/main/java/com/example/English4Kids_Backend/English4KidsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.English4Kids_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class English4KidsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(English4KidsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/English4Kids_Backend/English4KidsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.English4Kids_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class English4KidsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
